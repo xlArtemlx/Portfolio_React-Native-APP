104490801b25c5528111a54f05970232dd88275a
xlArtemlx <59087414+xlArtemlx@users.noreply.github.com>
2020-08-18 10:32:41 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {} from 'react-native';
+import {AppNavigation} from './src/Navigation/AppNavigation';
+
+export default function App() {
+
+  return <AppNavigation  />
+}
